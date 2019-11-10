c860cf28fbdea50100ed93ccc2a530ca95b77f46
oodennis20 <oodennis20@gmail.com>
2019-11-10 10:36:17 +0300

    add creation of user and authentification functions in main file

--- pswdLocker.py
+++ pswdLocker.py
@@ -19,7 +19,16 @@ def authenticate_user(firstname,password):
     confirm_user = Credential.confirm_user(first_name,password)
     return confirm_user
 
+def create_credential(user_name,social_media,account_name,password):
+    '''
+    Creating new credentials
+    '''
+    new_credential = Credential(user_name,social_media,account_name,password)
+    return new_credential
 
+def save_credential(credential):
+    Credential.save_credentials(credential)
+    
 
 if __name__ == '__main__': 
     main() 
