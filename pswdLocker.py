78d1011107e893bce25367d60829e7c24878b262
oodennis20 <oodennis20@gmail.com>
2019-11-10 12:36:33 +0300

    log in directions complete,that is to the credentials side

--- pswdLocker.py
+++ pswdLocker.py
@@ -40,8 +40,7 @@ def main():
     print('Hello!! Welcome to Password Locker.')
     while True:
         print('')
-        print("-"
-              *60)
+        print("-"*60)
         print('Use these codes to navigate: \n ca-Create Account \n li-Log In \n ex-Exit')
         short_code = input('Please enter your choice ').lower().strip ()
         
@@ -52,5 +51,45 @@ def main():
             print("-"*60)
             print(' ')
             print('Create New Account: ')
+            first_name = input ('Enter your first name- ').strip()
+            last_name = input ('Enter your last name- ').strip()
+            password = input ('Enter your password- ').strip()
+            save_user(create_user(first_name,last_name,password))
+            if user_exists == user_name:
+                print(" ")
+                print(f'Welcome {user_name}. PLease choose an option to proceed.')
+                print(' ')
+                while True:
+                    print("-"*60)
+                    print('Navigation codes: \n cc-Create a Credential \n dc-Display Credentials \n ex-Exit')
+                    short_code = input('Enter your choice: ').lower().strip()
+                    print("-"*60)
+                    if short_code == 'ex':
+                        print(" ")
+                        print(f'Goodbye {user_name}')
+                        break
+            elif short_code == 'cc':
+                            print(' ')
+                            print('Enter your credential details:')
+                            social_media = input('Enter the social media\'s name- ').strip()
+                            account_name = input('Enter your account\'s name - ').strip()
+                            while True:
+                                print(' ')
+                                print("-"*60)
+                                pswd_choice = input('Enter an option: ').lower().strip()
+                                print("-"*60)
+                                if pswd_choice == 'ep':
+                                    print(" ")
+                                    password = input('Enter your password: ').strip()
+                                    break     
+                                elif pswd_choice == 'gp':
+                                    password = generate_password()
+                                    break
+                                elif pswd_choice == 'ex':
+                                    break
+                                        
+
+                    
+            
 if __name__ == '__main__': 
     main() 
